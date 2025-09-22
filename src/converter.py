f3fe5bad5a4da5a254b2ded9fb7ebc8594bed81c
Conor <C.Barron4853@student.leedsbeckett.ac.uk>
2025-09-22 15:22:42 +0100

    Fixed bug caused by checking argv length after assigning to variable. Added error handling

--- src/converter.py
+++ src/converter.py
@@ -5,29 +5,38 @@ import pandas as pd
 import sys
 
 def csv_to_json(csv_file_path, json_file_path):
-    data = []
+    try:
+        data = []
 
-    with open(csv_file_path, encoding='utf-8') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            data.append(row)
+        with open(csv_file_path, encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                data.append(row)
 
-    with open(json_file_path, mode='w', encoding='utf-8') as json_file:
-        json.dump(data, json_file, indent=4)
+        with open(json_file_path, mode='w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, indent=4)
+
+    except Exception as e:
+        print(f"Error converting CSV to JSON: {e}")
+        sys.exit(1)
 
 def csv_to_excel(csv_file_path, excel_file_path):
-    df = pd.read_csv(csv_file_path)
-    df.to_excel(excel_file_path, index=False)
+    try:
+        df = pd.read_csv(csv_file_path)
+        df.to_excel(excel_file_path, index=False)
 
+    except Exception as e:
+        print(f"Error converting CSV to Excel: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
-    csv_file = sys.argv[1]
-    output_file_type = sys.argv[2].lower()
-    file_name = os.path.splitext(os.path.basename(csv_file))[0]
-
     if len(sys.argv) !=3:
         print("Usage: python converter.py <csv_file_path> <output_file_type (json|excel)>")
         sys.exit(1)
+
+    csv_file = sys.argv[1]
+    output_file_type = sys.argv[2].lower()
+    file_name = os.path.splitext(os.path.basename(csv_file))[0]
 
     if not os.path.exists(csv_file):
         print("File not found!")
